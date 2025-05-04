9c2afe47d24b3ac71bd689cf76e1c407a66781e7
Gaurav29062002 <gauravsanjaysonar81@gmail.com>
2025-05-04 21:36:09 +0530

    Added

--- server.js
+++ server.js
@@ -6,7 +6,4 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/contact', contactRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app; // for testing
+module.exports = app; // Only export the app
